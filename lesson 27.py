0b516ea36749982a2312b4172346f7a7d7ff9cf6
lisarchick <kochanov.ps@gmail.com>
2026-06-17 21:19:54 +0300

    newtwo

--- lesson 27.py
+++ lesson 27.py
@@ -13,11 +13,33 @@ def create_db():
                     )
     ''')
         print("SUCESFULY start")
-        conn.commit()
-        conn.close()
+
     except Exception as ex:
         print("dont start ERORR")
         print(ex)
 
+def create_user(name,login,age):
+    cursor.execute('INSERT INTO users (name, login, age) VALUES(?, ?, ?)',
+                   (name,login,age))
+                    
+    
+
+    conn.commit()
+def add_balance(balance,login):
+    cursor.execute('UPDATE users SET balance = ? WHERE login = ?', 
+                   (balance, login))
+    conn.commit()
+
 if __name__ == "__main__":
+    question = str(input("register or log in ?"))
     create_db()
+    if question == "register".lower():
+        name = str(input("your name: "))
+        login = str(input("your login: "))
+        age = int(input("your age: "))
+        create_user(name,login,age)
+        while True:
+            balance = float(input("add your balance sir: "))
+            add_balance(balance, login)
+    elif question == "log in".lower():
+        pass
