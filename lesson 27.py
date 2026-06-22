e27e317c6d2f91b38c381d63edf5e0795a454486
lisarchick <kochanov.ps@gmail.com>
2026-06-22 21:14:25 +0300

    new autorizati

--- lesson 27.py
+++ lesson 27.py
@@ -9,7 +9,8 @@ def create_db():
             name TEXT NOT NULL,
             login TEXT NOT NULL,
             age INTEGER,
-            balance REAL DEFAULT 0.0  
+            balance REAL DEFAULT 0.0 ,
+            password TEXT NOT NUll
                     )
     ''')
         print("SUCESFULY start")
@@ -18,9 +19,9 @@ def create_db():
         print("dont start ERORR")
         print(ex)
 
-def create_user(name,login,age):
-    cursor.execute('INSERT INTO users (name, login, age) VALUES(?, ?, ?)',
-                   (name,login,age))
+def create_user(name,login,age,password):
+    cursor.execute('INSERT INTO users (name, login, age, password) VALUES(?, ?, ?, ?)',
+                   (name,login,age, password))
                     
     
 
@@ -30,14 +31,18 @@ def add_balance(balance,login):
                    (balance, login))
     conn.commit()
 
-def autorixation():
+def autorixation(vhod):
     user_login = str(input("input log: "))
-    test = cursor.execute('SELECT id FROM users WHERE login = ?',
-                     (user_login, ))
-    if test.fetchone() is not None:
+    user_password = str(input('input password'))
+    test = cursor.execute('SELECT id FROM users WHERE login = ? AND password = ?',
+                     (user_login,user_password,  ))
+    if test.fetchone() is not None :
         print('hello')
+        vhod = True
+        return vhod
     else:
         ('i dont know who u are')
+        return vhod
 
 def create_product():
     try:
@@ -54,7 +59,7 @@ def create_product():
         print(ex)
 def add_product():
     name_products = str(input('input name of product: '))
-    price_products = str(input('input name of product: '))
+    price_products = float(input('input name of product: '))
     count_products = str(input("input count of products"))
     cursor.execute ("INSERT INTO product (name, price, count) VALUES (?, ?, ?)",
                     (name_products, price_products, count_products))
@@ -68,11 +73,28 @@ def find_product():
     rows = cursor.fetchall()
     if rows:
         for i in rows:       
-            print(i[1], i[2])
+            print(f" Name: {i[1]},\nprice: {i[2]}rub")
+            buy_product(find_name_product)
     else:
         print('i dont find this product')
         
-    
+def buy_product(find_name_product):
+    buy_product_quantity = int(input("input count of products: "))
+    cursor.execute("SELECT name, price, count FROM product WHERE name = ?",
+                   (find_name_product, ))
+    product = cursor.fetchone()
+    if not product:
+        print('product dont found')
+    else:
+        name,price,stock = product
+        if buy_product_quantity > stock:
+            print('nedostatocho products')
+        else:
+            new_stock = stock - buy_product_quantity
+            cursor.execute("UPDATE product SET count = ? WHERE name = ?", (new_stock,find_name_product))
+            conn.commit()
+            total_price = price * buy_product_quantity
+            print(f" sucessfuly buy{name}\nCount: {buy_product_quantity},\n{total_price:.2f}")
         
 
 if __name__ == "__main__":
@@ -82,17 +104,27 @@ if __name__ == "__main__":
         name = str(input("your name: "))
         login = str(input("your login: "))
         age = int(input("your age: "))
-        create_user(name,login,age)
+        password = str(input("your password: "))
+        create_user(name,login,age,password)
         while True:
             balance = float(input("add your balance sir: "))
             add_balance(balance, login)
     elif question == "log in".lower():
-            autorixation()
-            action = str(input("your move: "))
-            if action == "shop":
-                create_product()
-                add_my_product = str(input("add your product: ")). lower()
-                add_product()
-                find_product()
+            vhod = False
+            autorixation(vhod)
+            if vhod == False:
+                print("goodbuy")
+            else:
+                action = str(input("your move: "))
+                if action == "shop":
+                    create_product()
+                    add_my_product = str(input("add your product: ")). lower()
+                    if add_product == "yes":
+                        add_product()
+                    else:
+                        find_product()
+                    
+
+
                 
         
